6e3779bd0794be79dbd06c7f2f5a5117af6ef664
Sefinek <contact@sefinek.net>
2025-08-27 18:27:52 +0200

    Update index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,23 +1,28 @@
-declare module '@sefinek/email-validator' {
-    /**
-     * Function used for validating an email address.
-     *
-     * @param {string} email - The email address to validate.
-     * @example
-     * const emailValidator = require('@sefinek/email-validator');
-     * const email = 'example@example.com';
-     *
-     * if (emailValidator.test(email)) {
-     *    console.log(`Email ${email} is valid!`);
-     * } else {
-     *    console.log(`Email ${email} is NOT valid!`);
-     * }
-     * @returns {boolean} - Returns true if the email address is valid, otherwise false.
-     */
-    export function test(email: string): boolean;
+/**
+ * Function used for validating an email address.
+ *
+ * @param email - The email address to validate.
+ * @example
+ * const emailValidator = require('@sefinek/email-validator');
+ * const email = 'example@example.com';
+ *
+ * if (emailValidator.test(email)) {
+ *    console.log(`Email ${email} is valid!`);
+ * } else {
+ *    console.log(`Email ${email} is NOT valid!`);
+ * }
+ * @returns Returns true if the email address is valid, otherwise false.
+ */
+declare function test(email: string): boolean;
 
-    /**
-     * The version of the `email-validator` module.
-     */
-    export const version: string;
-}
+/**
+ * The version of the `email-validator` module.
+ */
+declare const version: string;
+
+declare const emailValidator: {
+    test: typeof test;
+    version: typeof version;
+};
+
+export = emailValidator;
